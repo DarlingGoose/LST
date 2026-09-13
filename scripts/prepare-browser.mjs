3f765fa2e736bb79b8c884c5fe4e11901dd6677b
DarlingGoose <seannmoser@gmail.com>
2026-09-14 08:11:47 +0900

    fix: add favicons

--- scripts/prepare-browser.mjs
+++ scripts/prepare-browser.mjs
@@ -29,6 +29,7 @@ export async function prepareBrowser(browser) {
     await cp(file, `${outputDirectory}/${file}`);
   }
   await cp("icons", `${outputDirectory}/icons`, { recursive: true });
+  await cp("favicons", `${outputDirectory}/favicons`, { recursive: true });
 
   const manifestPath = `${outputDirectory}/manifest.json`;
   const manifest = JSON.parse(await readFile(manifestPath, "utf8"));
